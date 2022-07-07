63ad4945672fd20c196a4cb238055c6cb013ef44
José Alvarado <josealvarado2117@gmail.com>
2022-07-06 23:27:16 -0400

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jrag-dev.github.io/buscador-imagenes-react/',
   server: {
     host: '0.0.0.0'     // ← new content ←
   },
